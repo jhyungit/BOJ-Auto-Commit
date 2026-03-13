6e63be3b1b0cfa2022c26078779de244ea25fa39
jhyungit <92933743+jhyungit@users.noreply.github.com>
2026-03-13 13:43:26 +0900

    [Gold IV] Title: 운동, Time: 532 ms, Memory: 58212 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.java
+++ 백준/Gold/1956. 운동/운동.java
@@ -36,16 +36,12 @@ public class Main {
 		// 플로이드 워셜 수행.
 		floydwarshell();
 		
-		int ans = Integer.MAX_VALUE;
-		for(int i = 1; i < V; i++) {
-			for(int j = i+1; j<=V; j++) {
-				if(graph[i][j] == INF || graph[j][i] == INF) continue;
-				//왕복 가능
-				ans = Math.min(graph[i][j]+graph[j][i], ans);
-			}
+		int ans = INF;
+		for(int i = 1; i <= V; i++) {
+          ans = Math.min(ans, graph[i][i]);
 		}
-		
-		System.out.println(ans != Integer.MAX_VALUE ? ans : -1);
+    	
+		System.out.println(ans != INF ? ans : -1);
 	}
 	
 	static void floydwarshell() {
